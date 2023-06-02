9ba6050d9326138b513ef6dfbcbcdac56fe0fec3
mubeenullhaq <mubeenullhaq@gmail.com>
2023-06-03 02:32:10 +0500

    authorization applied on Pool creation route as admin only

--- routes/pools.js
+++ routes/pools.js
@@ -1,11 +1,12 @@
 var express = require("express");
 const { Pools, validate } = require("../models/pool");
 const auth = require("../middleware/auth");
+const admin = require("../middleware/admin");
 const _ = require("lodash");
 var router = express.Router();
 
 //Create Pool
-router.post("/", async (req, res, next) => {
+router.post("/", [auth, admin],async (req, res, next) => {
   try {
     //return res.status(200).send("Working...");
     const { error } = validate(req.body);
@@ -32,79 +33,6 @@ router.get("/", async (req, res, next) =>{
   if(!pools) res.status(404).send("No Pools exist. Please create pool first.");
   return res.status(200).send(pools);
 })
-//Update User by ID
-router.put("/", [auth], async (req, res, next) => {
-  try {
-    const { error } = validateUpdate(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
-    let users = await User.findOneAndUpdate(
-      { _id: req.user._id },
-      _.pick(req.body, ["name", "email", "number", "location", "locationDesc"]),
-      { new: true }
-    );
-    if (!users) {
-      return res.status(400).send({ message: "No user found to Update" });
-    }
-    // users[req.body.name] = req.body.value;
-    // await users.save();
-    return res.status(200).send(users);
-  } catch (e) {
-    return res.send(e);
-  }
-});
 
-//Change blocked status of a User by ID
-router.put("/blockUser/:id", async (req, res, next) => {
-  try {
-    let user = await User.findOne({ _id: req.params.id });
-    if (!user) {
-      return res.status(400).send({ message: "No user found to Update" });
-    }
-    user.isBlocked = !user.isBlocked;
-    await user.save();
-    // users[req.body.name] = req.body.value;
-    // await users.save();
-    return res.status(200).send(user);
-  } catch (e) {
-    return res.send(e);
-  }
-});
 
-//Read Single User By ID
-router.get("/:id", async (req, res, next) => {
-  try {
-    let user = await User.findOne({ _id: req.params.id });
-    if (!user) {
-      return res.status(404).send({ message: "No user found" });
-    }
-    return res.status(200).send(user);
-  } catch (e) {
-    return res.send(e);
-  }
-});
-//Read All Users
-router.get("/", async (req, res, next) => {
-  try {
-    let users = await User.find();
-    if (!users) {
-      return res.status(400).send({ message: "No user found" });
-    }
-    return res.status(200).send(users);
-  } catch (e) {
-    return res.send(e);
-  }
-});
-
-//Delete a user by Id
-router.delete("/:id", async (req, res) => {
-  try {
-    const user = await User.findByIdAndRemove(req.params.id);
-
-    if (!user)
-      return res.status(404).send("The user with given id was not found...");
-    res.send(user);
-  } catch (e) {
-    return res.send(e);
-  }
-});
 module.exports = router;
