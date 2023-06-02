ea74c72e4452c72a4fed5929133f23bf2bd1377d
Zaheer ud Din Faiz <zaheer@tecknoworks.com>
2023-06-02 16:25:43 +0500

    staking route implementd

---
+++ routes/stakings.js
@@ -0,0 +1,103 @@
+var express = require("express");
+const { Stakings, validate } = require("../models/staking");
+const { Pools } = require("../models/pool");
+
+// const auth = require("../middleware/auth");
+const _ = require("lodash");
+var router = express.Router();
+
+//Create Staking
+router.post("/", async (req, res, next) => {
+  try {
+    //return res.status(200).send("Working...");
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+    let poolId = req.body.pool_id;
+    let pool = await Pools.findById(poolId);
+    if (!pool)
+      return res.status(404).send(`Pool Not found for the ID: ${poolId} `);
+
+    let staking = new Stakings(
+      _.pick(req.body, ["pool_id", "partner_id", "amount"])
+    );
+    staking.set({
+      created_at: Date.now(),
+      pool_info: {
+        name: pool.name,
+        profit: pool.profit,
+      },
+    });
+    await staking.save();
+
+    return res.status(200).send({
+      message: "Amount Staked Successfull",
+      staking,
+    });
+  } catch (e) {
+    return res.status(500).send(e.message);
+  }
+});
+
+//Read Single Pool By ID
+router.get("/:id", async (req, res, next) => {
+  try {
+    let pool = await Pools.findOne({ _id: req.params.id });
+    if (!pool) {
+      return res.status(404).send({ message: "No pool found" });
+    }
+    return res.status(200).send(pool);
+  } catch (e) {
+    return res.send(e);
+  }
+});
+
+//Read All Pools
+router.get("/", async (req, res, next) => {
+  try {
+    let pools = await Pools.find();
+    if (!pools) {
+      return res.status(400).send({ message: "No user found" });
+    }
+    return res.status(200).send(pools);
+  } catch (e) {
+    return res.send(e);
+  }
+});
+
+//Update Pool by ID
+// router.put("/", async (req, res, next) => {
+//   try {
+//     const { error } = validateUpdate(req.body);
+//     if (error) return res.status(400).send(error.details[0].message);
+//     let users = await User.findOneAndUpdate(
+//       { _id: req.user._id },
+//       _.pick(req.body, ["name", "email", "number", "location", "locationDesc"]),
+//       { new: true }
+//     );
+//     if (!users) {
+//       return res.status(400).send({ message: "No user found to Update" });
+//     }
+//     // users[req.body.name] = req.body.value;
+//     // await users.save();
+//     return res.status(200).send(users);
+//   } catch (e) {
+//     return res.send(e);
+//   }
+// });
+
+//Delete a Pool by Id
+router.delete("/:id", async (req, res) => {
+  try {
+    const pool = await Pools.findByIdAndRemove(req.params.id);
+
+    if (!pool)
+      return res.status(404).send("The pool with given id was not found...");
+    res
+      .status(200)
+      .send({ message: "Following Pool Successfully Deleted", Pool: pool });
+  } catch (e) {
+    return res.send(e.message);
+  }
+});
+
+module.exports = router;
