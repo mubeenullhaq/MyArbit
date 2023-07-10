e55bda83accbf31a876e4c107ba98e329b157bbe
mubeenullhaq <mubeenullhaq@gmail.com>
2023-07-10 23:22:21 +0500

    hide pool route implemented

--- routes/pools.js
+++ routes/pools.js
@@ -6,7 +6,7 @@ const _ = require("lodash");
 var router = express.Router();
 
 //Create Pool
-router.post("/", [auth, admin],async (req, res, next) => {
+router.post("/", [auth, admin], async (req, res, next) => {
   try {
     //return res.status(200).send("Working...");
     const { error } = validate(req.body);
@@ -17,9 +17,7 @@ router.post("/", [auth, admin],async (req, res, next) => {
     });
     if (pool) return res.status(400).send("Pool name alredy registered");
 
-    pool = new Pools(
-      _.pick(req.body, ["name", "min_stake", "duration", "profit"])
-    );
+    pool = new Pools(_.pick(req.body, ["name", "min_stake", "duration", "profit"]));
     await pool.save();
 
     return res.status(200).send(pool);
@@ -28,11 +26,32 @@ router.post("/", [auth, admin],async (req, res, next) => {
   }
 });
 //Read All Pools
-router.get("/", async (req, res, next) =>{
+router.get("/", async (req, res, next) => {
   const pools = await Pools.find();
-  if(!pools) res.status(404).send("No Pools exist. Please create pool first.");
+  if (!pools) res.status(404).send("No Pools exist. Please create pool first.");
   return res.status(200).send(pools);
-})
+});
 
+//Update Hidden Status
+router.put("/", [auth, admin], async (req, res, next) => {
+  try {
+    //return res.status(200).send("Working...");
+    if (!req.body.poolId) return res.status(400).send("Pool ID not found to update");
+
+    let pool = await Pools.findById(req.body.poolId);
+    if (!pool) return res.status(404).send("NOT_FOUND");
+    const updatedIsHidden = !pool.is_hidden;
+    pool.set({
+      is_hidden: !pool.is_hidden,
+    });
+    pool = await pool.save();
+    return res.status(200).send({
+      message: "Pool Updated Successfully",
+      pool: result,
+    });
+  } catch (e) {
+    return res.status(500).send(e.message);
+  }
+});
 
 module.exports = router;
