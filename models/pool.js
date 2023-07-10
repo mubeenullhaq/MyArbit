1493437bf595b712a846b9ffb842fd4f6d3c71bc
mubeenullhaq <mubeenullhaq@gmail.com>
2023-07-10 08:11:29 +0500

    pool schema updated

--- models/pool.js
+++ models/pool.js
@@ -3,17 +3,25 @@ var mongoose = require("mongoose");
 
 var poolsSchema = mongoose.Schema({
   name: String,
-  min_stake: Number,
+  min_stake: {
+    type: Number,
+    default: 50,
+  },
   duration: Number,
   profit: Number,
+  is_hidden: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 function validate(pool) {
   const schema = Joi.object({
     name: Joi.string().min(2).max(50).required(),
-    min_stake: Joi.number().required(),
+    min_stake: Joi.number(),
     duration: Joi.number().required(),
     profit: Joi.number().required(),
+    is_hidden: Joi.bool(),
   });
 
   return schema.validate(pool);
